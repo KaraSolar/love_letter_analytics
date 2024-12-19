1323db542bda541da52cbb66010dad04eaa09cec
danee593 <danee593@gmail.com>
2024-12-19 20:29:16 +0000

    dim_community, typos and tests

---
+++ models/marts/dim_community.sql
@@ -0,0 +1,26 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='community_id',
+        schema='marts',
+        on_schema_change='fail'
+    )
+}}
+
+SELECT
+    community_id
+    ,name
+    ,ST_GEOGPOINT(
+        SAFE_CAST(SUBSTR(coordinates, INSTR(coordinates, ',') + 1) AS NUMERIC), -- longitude
+        SAFE_CAST(SUBSTR(coordinates, 1, INSTR(coordinates, ',') - 1) AS NUMERIC) -- latitude
+        ) AS coordinates
+    ,region
+    ,population
+    ,households
+    ,healthcare_availability
+    ,is_active
+    ,updated_at
+FROM {{ ref('community')}} b
+{% if is_incremental() %}
+WHERE updated_at >= (SELECT COALESCE(MAX(updated_at),'1900-01-01') FROM {{ this }} )
+{% endif %}
